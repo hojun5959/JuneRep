a8d203643f7f633ce3c8a4d810a10535543ebb88
hojun5959 <hojun5959@snu.ac.kr>
2023-06-23 01:47:19 +0900

    Sangmo commit

---
+++ February ML/config.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+EXPERIMENT = 'indices' # Name of experiment
+RUN_NAME = 'initial assessment' # Name of iteration
+ARTIFACT_DIR = '../artifact_dir' # Dir for notebook outputs where plots are saved
+
